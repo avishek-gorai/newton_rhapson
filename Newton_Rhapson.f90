7d5a6ea306c9e2e87c53c6d1358ad2b1c72831c8
Avishek Gorai <avishekgorai@myyahoo.com>
2025-02-06 19:51:54 +0530

    Added FORMAT statements.

--- Newton_Rhapson.f90
+++ Newton_Rhapson.f90
@@ -28,27 +28,33 @@ PROGRAM newton_rhapson_method
 
     PRINT *, "Copyright (C) 2025 Avishek Gorai <avishekgorai@myyahoo.com>"
 
-    PRINT "(A1, A)", "0", "This program is free software: you can redistribute it and/or modify " // &
-         "it under the terms of the GNU General Public License as published by " // &
-         "the Free Software Foundation, either version 3 of the License, or " // &
-         "(at your option) any later version."
+    PRINT 10
+10  FORMAT ("0", "This program is free software: you can redistribute it and/or modify ", &
+         "it under the terms of the GNU General Public License as published by ", &
+         "the Free Software Foundation, either version 3 of the License, or ", &
+         "(at your option) any later version.")
 
-    PRINT "(A1, A)", "0", "This program is distributed in the hope that it will be useful, " // &
-         "but WITHOUT ANY WARRANTY; without even the implied warranty of " // &
-         "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the " // &
-         "GNU General Public License for more details."
+    PRINT 20
+20  FORMAT ("0", "This program is distributed in the hope that it will be useful, ", &
+         "but WITHOUT ANY WARRANTY; without even the implied warranty of ", &
+         "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the ", &
+         "GNU General Public License for more details.")
 
-    PRINT "(A1, A)", "0", "You should have received a copy of the GNU General Public License " // &
-         "along with this program.  If not, see <https://www.gnu.org/licenses/>."
-       
+    PRINT 30
+30  FORMAT ("0", "You should have received a copy of the GNU General Public License ", &
+         "along with this program.  If not, see <https://www.gnu.org/licenses/>.")
+
   input_loop: DO
-     PRINT "(A1, A)", "0", "Enter guess:-"
+     PRINT 40
+40   FORMAT ("0", "Enter guess:-")
      READ (*, *, iostat = input_status) x
-     
+
      IF (input_status /= 0) THEN
-        PRINT "(A1, A)", "0", "Incorrect input!"
+        PRINT 50
+50      FORMAT ("0", "Incorrect input!")
      ELSE IF (input_status == -1) THEN
-        PRINT "(A1, A)", "0", "Bye!"
+        PRINT 60
+60      FORMAT("0", "Bye!")
         EXIT
      ELSE
         converges = (f(x) * second_derivative_of_f(x)) < (derivative_of_f(x) ** 2)
@@ -64,13 +70,19 @@ PROGRAM newton_rhapson_method
                  number_of_iterations = number_of_iterations + 1
               END IF
            END DO
-           PRINT "('0Number of ierations = ', I10)", number_of_iterations
-           PRINT *, "x = ", x_new
+           PRINT 70
+70         FORMAT ("0", "Number of ierations:-")
+           PRINT *, number_of_iterations
+           PRINT 90
+90         FORMAT ("0", "Approximate root:-")
+           PRINT *, x_new
            EXIT
         ELSE
-           PRINT *, "The series will not converge for initial x = ", x
+           PRINT 80
+80         FORMAT ("0", "The series will not converge for the initial value:-")
+           PRINT *, x
            CYCLE
         END IF
      END IF
-  END DO input_loop 
+  END DO input_loop
 END PROGRAM newton_rhapson_method
